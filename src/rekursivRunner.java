6858a4c80e68019293fa71fc0a8fa32acb5c7953
l-reich <64188912+l-reich@users.noreply.github.com>
2024-04-21 09:38:21 +0200

    added recursive algorithm

--- src/rekursivRunner.java
+++ src/rekursivRunner.java
@@ -1,4 +1,4 @@
-public class naivRunner {
+public class rekursivRunner {
 
     private static int l;
     private static int r;
@@ -8,7 +8,7 @@ public class naivRunner {
 
         int[] testcase = {5, -2, 5, -2, 1, -9, 12, -2, 24, -5, 13, -12, 3, -13, 5, -3, 2, -1, 2};
         long startTime = System.nanoTime();
-        naive(testcase, testcase.length);
+        recursive(testcase, testcase.length);
         long endTime = System.nanoTime();
         long duration = (endTime - startTime);
         double microseconds = (double) duration / 1000;
@@ -17,16 +17,13 @@ public class naivRunner {
         System.out.println("Laufzeit: " + microseconds + " μs");
     }
 
-    public static void naive(int[] a, int n) {
+    public static void recursive(int[] a, int n) {
         maxscore = 0;
         l = 1;
         r = 0;
         for (int i = 0; i < n; i++) {
             for (int j = i; j < n; j++) {
-                int s = 0;
-                for (int k = i; k <= j; k++) {
-                    s = s + a[k];
-                }
+                int s = calculateScore(i, j, a);
                 if (s >= maxscore) {
                     maxscore = s;
                     l = i;
@@ -36,7 +33,17 @@ public class naivRunner {
         }
     }
 
-    public static String arrayStr(int[] arr){
+    public static int calculateScore(int i, int j, int[] arr) {
+        if (i > j) return 0;
+        else if (i == j) {
+            return arr[i];
+        } else {
+            int k = (i + j) / 2;
+            return calculateScore(i, k, arr) + calculateScore(k+1, j, arr);
+        }
+    }
+
+    public static String arrayStr(int[] arr) {
         StringBuilder sb = new StringBuilder();
         sb.append("[");
         for (int i = 0; i < arr.length; i++) {
