d0333dde37c5fbdc04bdc189c47ff516a2719db3
IngiumDev <86780096+IngiumDev@users.noreply.github.com>
2024-04-22 14:05:40 +0200

    Added utils

--- src/OptimalRunner.java
+++ src/OptimalRunner.java
@@ -2,18 +2,6 @@ import Model.MSSFinder;
 import Model.Subscore;
 
 public class OptimalRunner implements MSSFinder {
-    public static void main(String[] args) {
-        int[] testcase = {5, -2, 5, -2, 1, -9, 12, -2, 24, -5, 13, -12, 3, -13, 5, -3, 2, -1, 2};
-        long startTime = System.nanoTime();
-        OptimalRunner runner = new OptimalRunner();
-        Subscore result = runner.findMSS(testcase, testcase.length );
-        long endTime = System.nanoTime();
-        long duration = (endTime - startTime);
-        double microseconds = (double) duration / 1000;
-        System.out.println("Eingabe: " + arrayStr(testcase));
-        System.out.println("Ausgabe: " + "[" + result.getPair().getL() + "," + result.getPair().getR() + "] mit Score " + result.getScore());
-        System.out.println("Laufzeit: " + microseconds + " μs");
-    }
 
     public Subscore findMSS(int[] arr, int n) {
         int max = 0, l = 1, r = 0, rmax = 0, rstart = 1;
@@ -32,15 +20,4 @@ public class OptimalRunner implements MSSFinder {
         }
         return new Subscore(l, r, max);
     }
-    public static String arrayStr(int[] arr) {
-        StringBuilder sb = new StringBuilder();
-        sb.append("[");
-        for (int i = 0; i < arr.length; i++) {
-            sb.append(arr[i]);
-            if (i < arr.length - 1) {
-                sb.append(", ");
-            }
-        }
-        sb.append("]");
-        return sb.toString();
-    }}
+    }
---
+++ src/Utils.java
@@ -0,0 +1,14 @@
+public class Utils {
+    public static String arrayStr(int[] arr) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("[");
+        for (int i = 0; i < arr.length; i++) {
+            sb.append(arr[i]);
+            if (i < arr.length - 1) {
+                sb.append(", ");
+            }
+        }
+        sb.append("]");
+        return sb.toString();
+    }
+}
